cc5df03e6a34bbe6c83a93a0d40b5bc272b45a87
Darien Sessions <darien.sessions@gmail.com>
2025-01-12 12:46:27 -0500

    feat: update snake's data structure to better organize information

--- Easy Games/Snake Game/main.js
+++ Easy Games/Snake Game/main.js
@@ -1,46 +1,69 @@
 const snake = document.querySelector('.game-snake');
+const apple = document.querySelector('.game-apple');
 const gridColumns = 32;
 const gridRows = 24;
 
-let snakePosition = { x: 12, y: 12 };
-let snakeDirection = { x: 0, y: 0 };
-let speed = 200;
+const snakeData = {
+  length: 1,
+  position: { x: 12, y: 12 },
+  direction: { x: 0, y: 0 },
+  speed: 200,
+};
+
+let applePosition = { x: 18, y: 12 };
 
 function updateSnakePosition() {
-  snakePosition.x += snakeDirection.x;
-  snakePosition.y += snakeDirection.y;
+  snakeData.position.x += snakeData.direction.x;
+  snakeData.position.y += snakeData.direction.y;
 
-  if (snakePosition.x < 1) restartGame();
-  if (snakePosition.x > gridColumns) restartGame();
-  if (snakePosition.y < 1) restartGame();
-  if (snakePosition.y > gridRows) restartGame();
+  if (
+    snakeData.position.x < 1 ||
+    snakeData.position.x > gridColumns ||
+    snakeData.position.y < 1 ||
+    snakeData.position.y > gridRows
+  )
+    restartGame();
 
-  snake.style.gridColumn = `${snakePosition.x} / span 1`;
-  snake.style.gridRow = `${snakePosition.y} / span 1`;
+//   if (
+//     snakeData.position.x === applePosition.x &&
+//     snakeData.position.y === applePosition.y
+//   ) {
+//     snakeData.length += 1;
+//     apple.style
+//   }
+
+  snake.style.gridColumn = `${snakeData.position.x} / span 1`;
+  snake.style.gridRow = `${snakeData.position.y} / span 1`;
+}
+
+function setApplePosition() {
+  apple.style.gridColumn = `${applePosition.x} / span 1`;
+  apple.style.gridRow = `${applePosition.y} / span 1`;
 }
 
 function changeDirection(event) {
   switch (event.key) {
     case 'ArrowUp':
-      if (snakeDirection.y === 0) snakeDirection = { x: 0, y: -1 };
+      if (snakeData.direction.y === 0) snakeData.direction = { x: 0, y: -1 };
       break;
     case 'ArrowDown':
-      if (snakeDirection.y === 0) snakeDirection = { x: 0, y: 1 };
+      if (snakeData.direction.y === 0) snakeData.direction = { x: 0, y: 1 };
       break;
     case 'ArrowLeft':
-      if (snakeDirection.x === 0) snakeDirection = { x: -1, y: 0 };
+      if (snakeData.direction.x === 0) snakeData.direction = { x: -1, y: 0 };
       break;
     case 'ArrowRight':
-      if (snakeDirection.x === 0) snakeDirection = { x: 1, y: 0 };
+      if (snakeData.direction.x === 0) snakeData.direction = { x: 1, y: 0 };
       break;
   }
 }
 
 function restartGame() {
-  snakePosition = { x: 5, y: 10 };
-  snakeDirection = { x: 0, y: 0 };
+  setApplePosition();
+  snakeData.position = { x: 12, y: 12 };
+  snakeData.direction = { x: 0, y: 0 };
 }
 
 document.addEventListener('keydown', changeDirection);
 
-setInterval(updateSnakePosition, speed);
+setInterval(updateSnakePosition, snakeData.speed);
